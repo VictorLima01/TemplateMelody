7c0ae7fcd5b7669154a27f6ec8520efcd547a57f
VictorLima01 <victor.lima.araujo@gmail.com>
2020-04-07 18:00:44 -0300

    Arquivo para implementar a base de dados do db2.js

---
+++ melody/template/js/js-grid2.js
@@ -0,0 +1,70 @@
+(function($) {
+  'use strict';
+  $(function() {
+	  
+	  if ($("#js-grid-sortable2").length) {
+      $("#js-grid-sortable2").jsGrid({
+        height: "500px",
+        width: "100%",
+		filtering: true,
+        editing: true,
+        inserting: true,
+        sorting: true,
+        paging: true,
+        autoload: true,
+		autoload: true,
+        selecting: false,
+        pageSize: 15,
+        pageButtonCount: 5,
+        deleteConfirm: "Do you really want to delete the client?",
+		 data: db.clients,
+		controller: db,
+
+        fields: [{
+            name: "Nome",
+            type: "text",
+            width: 150
+          },
+          {
+            name: "CNPJ",
+            type: "text",
+            width: 200
+          },
+          {
+            name: "Segmento",
+            type: "select",
+			 items: db.countries,
+            valueField: "Id",
+            textField: "Name",
+			width: 100
+            
+          },
+          {
+            name: "Razão Social",
+            type: "text",
+			width: 150
+          },
+          {
+            name: "canal-distribuicao",
+            title: "Canal de distribuição",
+			type: "text",
+			width: 200
+          },
+		
+		  {
+            type: "control"
+          }
+		  
+        ]
+      });
+    }
+
+    if ($("#sort").length) {
+      $("#sort").on("click", function() {
+        var field = $("#sortingField").val();
+        $("#js-grid-sortable2").jsGrid("sort", field);
+      });
+    }
+  
+   });
+})(jQuery);
