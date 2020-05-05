c5b6cf07b6e89af37a80a2271213f0f62bf4781c
VictorLima01 <victor.lima.araujo@gmail.com>
2020-05-05 18:21:35 -0300

    Ajustes

--- melody/template/js/datepickers-splliter.js
+++ melody/template/js/datepickers-splliter.js
@@ -1,5 +1,14 @@
 $( "#datepicker-popup2" ).datepicker({
 	inline: true
 });
+$( "#datepicker-popup3" ).datepicker({
+	inline: true
+});
+$( "#datepicker-popup4" ).datepicker({
+	inline: true
+});
+$( "#datepicker-popup5" ).datepicker({
+	inline: true
+});
 
 
