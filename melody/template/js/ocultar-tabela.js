5d601c24cfd2c6997eea072fa65726abf2c7eb2d
VictorLima01 <victor.lima.araujo@gmail.com>
2020-05-07 17:40:05 -0300

    Ocultar colunas das tabelas

---
+++ melody/template/js/ocultar-tabela.js
@@ -0,0 +1,42 @@
+
+	$("#ocultar").click(function () {
+		$("#exemplo1").hide();
+    });
+    
+	$("#ocultar").click(function () {
+		$("#exemplo2").hide();
+    });
+    
+	$("#ocultar").click(function () {
+		$("#exemplo3").hide();
+    });
+    $("#ocultar").click(function () {
+		$("#exemplo4").hide();
+    });
+    $("#ocultar").click(function () {
+		$("#exemplo5").hide();
+    });
+    $("#ocultar").click(function () {
+		$("#exemplo6").hide();
+    });
+    
+    
+    $("#apresentar").click(function () {
+		$("#exemplo1").show();
+    });
+    $("#apresentar").click(function () {
+		$("#exemplo2").show();
+    });
+    $("#apresentar").click(function () {
+		$("#exemplo3").show();
+    });
+    $("#apresentar").click(function () {
+		$("#exemplo4").show();
+    });
+    $("#apresentar").click(function () {
+		$("#exemplo5").show();
+    });
+    $("#apresentar").click(function () {
+		$("#exemplo6").show();
+    });
+    
