a9bea1e766a5d4fd021543bc9139d55f2a24da53
VictorLima01 <victor.lima.araujo@gmail.com>
2020-05-14 18:33:40 -0300

    Adição de apagar e adicionar novos

--- melody/template/js/gravar.js
+++ melody/template/js/gravar.js
@@ -1,3 +1,4 @@
+/// Grava o nome e a descrição da atividade dentro da Div
 function gravar(){
     var titulo = document.getElementById("txtTitulo").value;
     var subtitulo = document.getElementById("txtSubtitulo").value;
@@ -5,11 +6,36 @@ function gravar(){
     
      
     div. innerHTML = "<h6>" + titulo +"</h6>"+ "\n" + "<p>" + subtitulo + "</p>";
+    console.log(div);
+
 }
+///  Colocar label de options dos produtos
 function select(){
     var nome = document.getElementById("txtNome").value;
     var option = document.getElementById("divSelect");
     
      
     option. innerHTML = "<h6>" + nome +"</h6>"; 
+}
+//// Duplica a div
+function duplicarCampos(){
+
+	var clone = document.getElementById('origem').cloneNode(true);
+	var destino = document.getElementById('destino');
+	destino.appendChild (clone);
+	
+	var camposClonados = clone.getElementsByTagName('input');
+	
+	 
+	for(i=0; i<camposClonados.length;i++){
+		camposClonados[i].getElementById("fone").value;
+	}
+	
+	
+	
+}
+
+function removerCampos(id){
+	var node1 = document.getElementById('destino');
+	node1.removeChild(node1.childNodes[0]);
 }
