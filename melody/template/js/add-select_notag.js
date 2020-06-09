9178749b4769c1b1537bdcf2dffb7521e050f8d7
VictorLima01 <victor.lima.araujo@gmail.com>
2020-06-09 15:50:41 -0300

    Add de select sem Tag

---
+++ melody/template/js/add-select_notag.js
@@ -0,0 +1,63 @@
+
+//// Adição de selects com tags
+
+$(".js-example-tags").select2({
+    tags: true
+  });
+  $('.js-example-tags').on("change", function (e) {
+  
+    
+  
+  
+    var valor = $(this).val();
+    if (valor == "produto") {
+      $('#id-da-div').hide();
+    } 
+    else {
+      $('#id-da-div').show();
+    }
+    
+    log("change");
+  });
+  
+  console.log(".js-example-tags");
+  //// Buscar
+  
+  function matchStart(params, data) {
+  
+    // If there are no search terms, return all of the data
+    if ($.trim(params.term) === '') {
+      return data;
+    }
+  
+    // Skip if there is no 'children' property
+    if (typeof data.children === 'undefined') {
+      return null;
+    }
+  
+    // `data.children` contains the actual options that we are matching against
+    var filteredChildren = [];
+    $.each(data.children, function (idx, child) {
+      if (child.text.toUpperCase().indexOf(params.term.toUpperCase()) == 0) {
+        filteredChildren.push(child);
+      }
+    });
+  
+    // If we matched any of the timezone group's children, then set the matched children on the group
+    // and return the group object
+    if (filteredChildren.length) {
+      var modifiedData = $.extend({}, data, true);
+      modifiedData.children = filteredChildren;
+  
+      // You can return modified objects from here
+      // This includes matching the `children` how you want in nested data sets
+      return modifiedData;
+    }
+  
+    // Return `null` if the term should not be displayed
+    return null;
+  }
+  
+  $(".js-example-matcher-start").select2({
+    matcher: matchStart
+  });
