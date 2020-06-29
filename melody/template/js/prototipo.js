1b309deeb2637758813553308270f82a18b5d203
VictorLima01 <victor.lima.araujo@gmail.com>
2020-06-29 12:55:35 -0300

    Página de teste

--- melody/template/js/prototipo.js
+++ melody/template/js/prototipo.js
@@ -65,5 +65,119 @@
             var ctx = document.getElementById('polar');
             window.myPolarArea = Chart.PolarArea(ctx, config);
         };
+//
+if ($('#sales-chart').length) {
+    var lineChartCanvas = $("#sales-chart").get(0).getContext("2d");
+    var data = {
+      labels: ["2013", "2014", "2014", "2015", "2016", "2017", "2018"],
+      datasets: [
+        {
+          label: 'Support',
+          data: [1500, 7030, 1050, 2300, 3510, 6800, 4500],
+          borderColor: [
+            '#392c70'
+          ],
+          borderWidth: 3,
+          fill: false
+        },
+        {
+          label: 'Product',
+          data: [5500, 4080, 3050, 5600, 4510, 5300, 2400],
+          borderColor: [
+            '#d1cede'
+          ],
+          borderWidth: 3,
+          fill: false
+        }
+      ]
+    };
+    var options = {
+      scales: {
+        yAxes: [{
+          gridLines: {
+            drawBorder: false
+          },
+          ticks: {
+            stepSize: 2000,
+            fontColor: "#686868"
+          }
+        }],
+        xAxes: [{
+          display: false,
+          gridLines: {
+            drawBorder: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      },
+      elements: {
+        point: {
+          radius: 3
+        }
+      },
+      stepsize: 1
+    };
+    var lineChart = new Chart(lineChartCanvas, {
+      type: 'line',
+      data: data,
+      options: options
+    });
+  }
+  if ($("#daily-sales-chart").length) {
+    var dailySalesChartData = {
+      datasets: [{
+        data: [50, 10, 10, 30],
+        backgroundColor: [
+          '#392c70',
+          '#04b76b',
+          '#e9e8ef',
+          '#ff5e6d'
+        ],
+        borderWidth: 0
+      }],
+  
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: [
+        'Mail order sales',
+        'Instore sales',
+        'Download sales',
+        'Sales return'
+      ]
+    };
+    var dailySalesChartOptions = {
+      responsive: true,
+      maintainAspectRatio: true,
+      animation: {
+        animateScale: true,
+        animateRotate: true
+      },
+      legend: {
+        display: false
+      },
+      legendCallback: function(chart) { 
+        var text = [];
+        text.push('<ul class="legend'+ chart.id +'">');
+        for (var i = 0; i < chart.data.datasets[0].data.length; i++) {
+          text.push('<li><span class="legend-label" style="background-color:' + chart.data.datasets[0].backgroundColor[i] + '"></span>');
+          if (chart.data.labels[i]) {
+            text.push(chart.data.labels[i]);
+          }
+          text.push('</li>');
+        }
+        text.push('</ul>');
+        return text.join("");
+      },
+      cutoutPercentage: 70     
+    };
+    var dailySalesChartCanvas = $("#daily-sales-chart").get(0).getContext("2d");
+    var dailySalesChart = new Chart(dailySalesChartCanvas, {
+      type: 'doughnut',
+      data: dailySalesChartData,
+      options: dailySalesChartOptions
+    });
+    document.getElementById('daily-sales-chart-legend').innerHTML = dailySalesChart.generateLegend();
+  }
 
        
