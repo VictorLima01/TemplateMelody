d8e0044f059c7493293f8f7c246c33678d527903
VictorLima01 <victor.lima.araujo@gmail.com>
2020-08-14 14:33:00 -0300

    JS para duplicar cards

---
+++ melody/template/js/duplicar-cards.js
@@ -0,0 +1,21 @@
+function duplicarCampos(){
+   
+    var clone = document.getElementById('origem').cloneNode(true);
+    var destino = document.getElementById('destino');
+    destino.appendChild (clone);
+    
+    // var camposClonados = clone.getElementsByTagName('input');
+    
+     
+    for(i=0; i<camposClonados.length;i++){
+        camposClonados[i].getElementById("fone").value;
+    }
+    
+    
+    
+}
+
+function removerCampos(id){
+    var node1 = document.getElementById('destino');
+    node1.removeChild(node1.childNodes[0]);
+}
